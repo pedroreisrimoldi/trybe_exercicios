f8f5f88e894890820db9fb6cbdb3188f8bdb3f5e
pedro reis rimoldi <pedroreisrimoldi@gmail.com>
2022-07-21 16:29:01 -0300

    dia8

---
+++ fundamentos_web/bloco8/dia8.1/fixacao/scripts.js
@@ -0,0 +1,7 @@
+const acordar = () => `Acordando`;
+const cafe = () => `Tomar café da manhã!`;
+const dormir = () => `Partiu dormir`;
+
+const fazer = (func) => func;
+
+console.log(fazer(dormir()));
